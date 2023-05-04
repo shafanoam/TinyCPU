d78dc9e1573e44b05f4ae539f9146bc7d7b8ffa3
shafanoam <109098752+shafanoam@users.noreply.github.com>
2023-05-04 14:10:39 -0700

    Started the actual assembling system

--- TCASM.py
+++ TCASM.py
@@ -1,12 +1,72 @@
+import time
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
+from tkinter.messagebox import showerror, showwarning, showinfo
+from os import path
+
 
 # done for fixing the blurriness issue
 import ctypes
 
 # fixes blurriness
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+# declare lists
+inputList = []
+
+
+def load_file(filepath):
+
+    global currentTask
+    currentTask = 'Loading files...'
+    taskLabel.configure(text='Loading files...')
+    specificProgress.configure(mode='indeterminate')
+    specificProgress.start()
+    window.update()
+
+    filepath = filepath.replace('\n', '')
+
+    print(filepath)
+
+    if path.exists(filepath):
+
+        with open(filepath, 'r') as file:
+            global inputList
+            for line in file:
+                # Remove newlines at the same time as adding each line
+                inputList.append(line.replace('\n', ''))
+                print(line)
+            file.close()
+
+        specificProgress.stop()
+        specificProgress.configure(mode='determinate')
+        overallProgress['value'] = 10
+        return True
+
+    else:
+        specificProgress.stop()
+        specificProgress.configure(mode='determinate')
+        specificProgress['value'] = 0
+        return False
+
+
+def start_system():
+
+    global currentTask
+    currentTask = 'Starting system...'
+    taskLabel.configure(background='red', text='Starting system...')
+    window.update()
+
+    input_path = inputPathLabel.get('1.0', 'end')
+    if not load_file(input_path):
+        showerror('Fatal Error', 'Input file does not exist!')
+
+
+
+    currentTask = 'Waiting for user...'
+    taskLabel.configure(background='light green', text='Waiting for user...')
+
 
 # initalize main window
 window = tk.Tk()
@@ -65,7 +125,7 @@ outputPathLabel['state'] = 'disabled'
 outputPathLabel.place(relx=0.17, rely=0.05, relwidth=0.81, relheight=0.7)
 
 
-def select_input_file():
+def select_output_file():
     filetypes = (('text files', '*.txt'), ('TCASM files', '*.tcasm'))
 
     file = fd.askopenfilename(title='Select the output file:',
@@ -77,7 +137,7 @@ def select_input_file():
     return
 
 
-outputPathButton = ttk.Button(outputArea, text='Browse', command=select_input_file)
+outputPathButton = ttk.Button(outputArea, text='Browse', command=select_output_file)
 outputPathButton.place(relx=0.01, rely=0.05, relwidth=0.15)
 
 
@@ -92,7 +152,7 @@ assembleArea.place(relx=0.01, rely=0.775, relwidth=0.98, height=90)
 
 currentTask = 'Waiting for user...'
 
-taskLabel = tk.Label(assembleArea, text=currentTask, background='light green', justify=tk.CENTER)
+taskLabel = tk.Label(assembleArea, text='Waiting for user...', background='light green', justify=tk.CENTER)
 taskLabel.place(relx=0.6, rely=0.01, relwidth=0.35)
 
 specificProgress = ttk.Progressbar(assembleArea, orient='horizontal', mode='determinate', length=100)
@@ -100,5 +160,12 @@ specificProgress.place(relx=0.02, rely=0.01, relwidth=0.55, relheight=0.35)
 
 overallProgress = ttk.Progressbar(assembleArea, orient='horizontal', mode='determinate', length=100)
 overallProgress.place(relx=0.02, rely=0.5, relwidth=0.55, relheight=0.35)
+
+startButton = ttk.Button(assembleArea, text='Assemble', command=start_system)
+startButton.place(relx=0.6, rely=0.5, relwidth=0.17)
+
+cancelButton = ttk.Button(assembleArea, text='Cancel')
+cancelButton.state(['disabled'])
+cancelButton.place(relx=0.78, rely=0.5, relwidth=0.17)
 
 window.mainloop()

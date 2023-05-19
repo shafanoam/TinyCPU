8180dee2dcecf2e955f897501dd942b0e2a9cced
shafanoam <109098752+shafanoam@users.noreply.github.com>
2023-05-19 10:11:18 -0700

    Deleted VDHL directory
    It was no longer necessary since I had given up getting logisim to play nice with the FPGA

--- VDHL/scripts/vivadoGenerateBitStream.tcl
+++
@@ -1,7 +0,0 @@
-open_project -verbose C:/Users/Michael/logisim_evolution_workspace/Noam_FPGA_CPU/main/sandbox//vp/vp.xpr
-update_compile_order -fileset sources_1
-launch_runs synth_1
-wait_on_run synth_1
-launch_runs impl_1 -to_step write_bitstream -jobs 8
-wait_on_run impl_1
-exit
--- VDHL/scripts/vivadoLoadBitStream.tcl
+++
@@ -1,9 +0,0 @@
-open_hw
-connect_hw_server
-open_hw_target
-set_property PROGRAM.FILE {C:/Users/Michael/logisim_evolution_workspace/Noam_FPGA_CPU/main/sandbox//vp/vp.runs/impl_1/LogisimToplevelShell.bit} [lindex [get_hw_devices] 0]
-current_hw_device [lindex [get_hw_devices] 0]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
-program_hw_device [lindex [get_hw_devices] 0]
-close_hw
-exit
